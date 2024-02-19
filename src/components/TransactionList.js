99b77b5dc16473a5caec9649bbf17dc92622d400
achrafbmr <dawddawd555@gmail.com>
2024-02-19 21:59:12 +0100

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import Transactions from './Transactions';
 
+
+//this function return all transactions 
 export default function TransactionList() {
     const { transactions } = useContext(GlobalContext);
 
